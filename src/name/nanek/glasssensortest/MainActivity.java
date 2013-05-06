41dfe6137bf9bbacb8d5f699afd52ea01dc96471
lance_nanek <lance_nanek@htc.com>
2013-05-06 15:14:16 -0700

    Initial commit.

---
+++ src/name/nanek/glasssensortest/MainActivity.java
@@ -0,0 +1,17 @@
+package name.nanek.glasssensortest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Log.i("GlassSensorTest", "onCreate");
+	}
+
+}
